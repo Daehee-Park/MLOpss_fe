f858a5b0454b649b3dfe2af09f25bce348567f40
Daehee-Park <qkreogml0@naver.com>
2023-12-24 16:32:46 +0900

    33

--- src/routes/preprocess/data_quality_check.svelte
+++ src/routes/preprocess/data_quality_check.svelte
@@ -5,18 +5,17 @@
     import Spinner from "./spinner.svelte";
 
     let loading = false;
-    let chartImage = '';
+    let chartImages = '';
 
     async function get_chart() {
         loading = true;
-        const response = await fetch('http://localhost:8000/get/chartData', {
+        const response = await fetch('http://localhost:8000/get/chart', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ data: data })
         });
-
         const responseData = await response.json();
-        chartImage = 'data:image/png;base64,' + responseData.chart;
+        chartImages = responseData.charts.map(chart => 'data:image/png;base64,' + chart);
         loading = false;
     }
 
@@ -24,20 +23,25 @@
         get_chart();
     });
 </script>
-
-<Dataframe data={data} />
-{#if loading}
-    <div class="flex flex-col items-center justify-center mt-12">
-        <Spinner />
-    </div>
-{:else}
+{#if !loading}
 <div>
     <button class="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600"
         on:click={handleNext}>
         Next
     </button>
 </div>
-<div class="flex flex-col mt-8 max-h-96">
-    <img src={chartImage} alt="Chart" />
+{/if}
+<Dataframe data={data} />
+{#if loading}
+    <div class="flex flex-col items-center justify-center mt-12">
+        <Spinner />
+    </div>
+{:else}
+<div class="grid grid-cols-4 mt-12">
+    {#each chartImages as chartImage}
+    <div class="max-w-full overflow-hidden">
+        <img src={chartImage} alt="Chart" style="width: 100%; height: auto;" />
+    </div>
+    {/each}
 </div>
 {/if}
